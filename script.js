a655610db1248cc45d338e093119d1b956bb301f
Daryna Toporovska <Daryna.Toporovska@student.souepl.cz>
2024-10-18 11:12:16 +0200

    done

---
+++ script.js
@@ -0,0 +1,6 @@
+function showImage() {
+    const imageUrl = document.getElementById('imageUrl').value;
+    const imgElement = document.getElementById('displayImage');
+    imgElement.src = imageUrl;
+}
+
